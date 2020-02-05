481e8d8391546397f38ec8b00eaf962d7961b684
jitesh456 <jitesh0dubey@gmail.com>
2020-02-05 18:00:53 +0530

    ADDED:Initialized and displayed board

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -1,2 +1,26 @@
-#!/bin/bash -x
+#!/bin/bash 
 echo "Welcome to Tic tac toe"
+declare -A board
+function	initializeBoard()
+{
+	for((row=0;row<3;row++))
+	do
+		for((column=0;column<3;column++))
+		do
+			board[$row,$column]="-"
+		done
+	done
+}
+function	displayBoard()
+{
+	for((row=0;row<3;row++))
+	do
+		for((column=0;column<3;column++))
+		do
+			echo -n ${board[$row,$column]} " "
+		done
+		echo -e
+	done
+}
+initializeBoard
+displayBoard

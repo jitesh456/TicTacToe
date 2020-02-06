433e06737ac857c557b767c046588acfe2d74c6f
jitesh456 <jitesh0dubey@gmail.com>
2020-02-06 19:19:55 +0530

    WIP:added computer as 2nd player

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -1,22 +1,24 @@
 #!/bin/bash  
- 
 #constant
 X=1
 O=1
+ROW=3
+COLUMN=3
 ALL_CELL_OCCUPIED=9
 #variable
 player="O"
+computer="O"
 flag=0
 move=0
-
+turn=0
 echo "Welcome to Tic tac toe"
 #Initialize Board
 declare -A board
 function initializeBoard()
 {
-	for((row=0;row<3;row++))
+	for((row=0;row<$ROW;row++))
 	do
-		for((column=0;column<3;column++))
+		for((column=0;column<$COLUMN;column++))
 		do
 			board[$row,$column]="-"
 		done
@@ -25,9 +27,9 @@ function initializeBoard()
 #displayBoard
 function displayBoard()
 {
-	for((row=0;row<3;row++))
+	for((row=0;row<$ROW;row++))
 	do
-		for((column=0;column<3;column++))
+		for((column=0;column<$COLUMN;column++))
 		do
 			echo -n ${board[$row,$column]} " "
 		done
@@ -37,12 +39,13 @@ function displayBoard()
 #Assign letter and decide who will play first
 function toss()
 {
-	if(( $((RANDOM %2)) == 1))
+	if(( $((RANDOM %2)) == $X ))
 	then
 		player="X"
 		echo "player will play first"
 	else
-		player="O"
+		computer="X"
+		echo "computer will play first"
 	fi
 }
 #check Empty cell
@@ -51,6 +54,7 @@ function isEmpty()
 	if [[ ${board[$1,$2]} == "-" ]]
 	then
 		flag=1
+		((move++))
 	fi
 }
 #check winner
@@ -58,74 +62,116 @@ function checkWinner()
 {
 	leftDiagonal=0
 	rightDiagonal=0
-	for((row=0;row<3;row++))
+	sign=$1
+	for((row=0;row<ROW;row++))
 	do
 		rowCount=0
 		columnCount=0
-		for((column=0;column<3;column++))
+		for((column=0;column<COLUMN;column++))
 		do
 #row check
-			if [[ ${board[$row,$column]} == $player ]]
+			if [[ ${board[$row,$column]} == $sign ]]
 			then
 				((rowCount++))
 			fi
 #col check
-			if [[ ${board[$column,$row]} == $player ]]
+			if [[ ${board[$column,$row]} == $sign ]]
 			then
 				((columnCount++))
 			fi
 #rightDiagonal
-         if [[ $row == $column && ${board[$row,$column]} == $player ]]
+         if [[ $row == $column && ${board[$row,$column]} == $sign ]]
          then
             ((rightDiagonal++))
 			fi
 #left Diagonal
-		   if [[ $(($row + $column))==2 && ${board[$row,$column]} == $player ]]
-         then
-            ((leftDiagonal++))
-         fi
-			if [[ $rowCount -eq 3 || $columnCount -eq 3 || $rightDiagonal -eq 3 || $leftDiagonal -eq 3 ]]
+			if [[ $(($row + $column))==$ROW && ${board[$row,$column]} == $sign ]]
 			then
-				echo "Winner"
+				((leftDiagonal++))
+			fi
+			if [[ $rowCount -eq $COLUMN || $columnCount -eq $COLUMN || $rightDiagonal -eq $COLUMN || $leftDiagonal -eq $COLUMN ]]
+			then
+				echo $sign :"Winner"
 				exit
 			fi
 		done
 	done
 }
 # for player  move
+function playMove()
+{
+#check  is empty
+	isEmpty $1 $2
+	if [[ $flag == 1 ]]
+	then
+		board[$row,$column]=$3
+	fi
+}
+function playFirst()
+{
+	if [[ $player == "X" ]]
+	then
+		playerMove
+		turn=$computer
+	else
+		computerMove
+		turn=$player
+	fi
+	displayBoard
+}
 function playerMove()
 {
-		while (( $flag != 1))
-		do
-			read -p "provide  row no like 0,1,2" row
-			read -p "provide  col no like 0,1,2" column
-			if(( $row > 2 && $column > 2))
-			then
-				echo "Not a valid row or column"
-			else
-#check empty  cell
-				isEmpty $row $column
-			fi
-			if (($flag == 1 ))
-			then
-				board[$row,$column]=$player
-				((move++))
-				flag=0
-			else
-				echo "NOT A EMPTY CELL"
-				flag=0
-			fi
-			displayBoard
-			checkWinner
-			if (($ALL_CELL_OCCUPIED == $move))
-			then
-				echo "tie"
-				exit
-			fi
-		done
+	read -p "provide  row no like 0,1,2" row
+	read -p "provide  col no like 0,1,2" column
+	#check for valid condiation
+	if (( $row > $ROW && $column > $COLUMN ))
+	then
+		echo "Not a valid row or column"
+	fi
+		playMove $row $column $player
 }
+function computerMove()
+{
 
+	row=$((RANDOM%3))
+	column=$((RANDOM%3))
+	isEmpty $row $column
+	if [[ $flag == 1 ]]
+	then
+		playMove $row $column $computer
+	fi
+}
+function playFirst()
+{
+	if [[ $player == "X" ]]
+	then
+		playerMove
+		turn=$computer
+	else
+		computerMove
+		turn=$player
+	fi
+}
 initializeBoard
-displayBoard
 toss
-playerMove
+playFirst
+while (( $move != $ALL_CELL_OCCUPIED ))
+do
+	if [[ $turn == $player ]]
+	then
+		playerMove
+		turn=$computer
+	else
+		echo "Computer Played"
+		computerMove
+		turn=$player
+	fi
+	displayBoard
+	checkWinner $computer
+	checkWinner $player
+	if (($ALL_CELL_OCCUPIED == $move))
+	then
+		echo "tie"
+		exit
+	fi
+done

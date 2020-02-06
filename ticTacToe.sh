d9fb911e4feea1f392491e58c620c973deaffddb
jitesh456 <jitesh0dubey@gmail.com>
2020-02-06 10:38:13 +0530

    ADDED: feature for checking valid cell

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -1,7 +1,12 @@
-#!/bin/bash  -x
+#!/bin/bash 
 #constant
 X=1
 O=1
+ALL_CELL_OCCUPIED=9
+#variable
+player="O"
+flag=0
+move=0
 echo "Welcome to Tic tac toe"
 declare -A board
 function initializeBoard()
@@ -35,7 +40,44 @@ function toss()
 		player="O"
 	fi
 }
+function isEmpty()
+{
+	if [[ ${board[$1,$2]} == "-" ]]
+	then
+		flag=1
+	fi
+}
+function playerMove()
+{
+		while (( $flag != 1))
+		do
+			read -p "provide  row no like 0,1,2" row
+			read -p "provide  col no like 0,1,2" column
+			if(( $row > 2 && $column > 2))
+			then
+				echo "Not a valid row or column"
+			else
+				isEmpty $row $column
+			fi
+			if (($flag == 1 ))
+			then
+				board[$row,$column]=$player
+				((move++))
+				flag=0
+			else
+				echo "NOT A EMPTY CELL"
+				flag=0
+			fi
+
+			displayBoard
+
+			if(($ALL_CELL_OCCUPIED == $move))
+			then
+				exit
+			fi
+		done
+}
 initializeBoard
 displayBoard
 toss
-
+playerMove
